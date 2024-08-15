00379511ca0c090f9f71d0c65026edbff4d6ca92
Farhan Khan <farnsolo@hotmail.com>
2024-08-15 22:34:40 +0100

    humidity code added

---
+++ humiditySensor/humiditySensor.ino
@@ -0,0 +1,40 @@
+#include <DHT.h>
+
+#define DHTPIN A0
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup() {
+  // Establishes serial communication between the arduino and another device
+  Serial.begin(9600);
+
+  delay(500);
+
+  Serial.println("DHT11 Humidity + temperature sensor\n\n");
+
+  dht.begin();  
+  delay(5000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if(isnan(h) || isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print("%\n");
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print("C\n");
+
+  delay(10000);
+}
