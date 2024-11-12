b503d6434943d2da30ca2d1df35a83b08c481bcf
Khan Farhan (2023) ZMAC533 <farhan.khan.2023@live.rhul.ac.uk>
2024-11-12 13:53:04 +0000

    Created fully send method and constructor - some errors to hash out

--- Transfer.h
+++ Transfer.h
@@ -8,27 +8,38 @@
 class Transfer
 {
   public:
-     myClass()
+     Transfer()
      {
        clientSocket = socket(AF_INET, SOCK_STREAM,0);
        
        serverAddress.sin_family = AF_INET;
        serverAddress.sin_port = htons(8080);
        
-       if (inet_pton(AF_INET,"172.28.91.42", &serverAddress.sin_addr) <= 0)
-       {
-         return -1;
-       }
+       //if (inet_pton(AF_INET,"172.28.91.42", &serverAddress.sin_addr) <= 0)
      }
      
      void send(float humidity, float temp)
      {
        if (connect(clientSocket, (struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0)
        {
-         return -1;
+         std::cout << "Error connecting to server - connection not made";
        }
-       const char* dataH;
-       const char* dataT;
-       int convert = snprinf(dataH. sizeof dataH. "%f", humidity);
+ 
+       const char* dataH = (char*)malloc(20);
+       const char* dataT = (char*)malloc(20);
+       int convert = snprintf(dataH, sizeof dataH, "%f", humidity);
+       int convert = snprintf(dataT,sizeof dataT)
+
+       char message[100];
+       strcpy(message, dataH);
+       free(dataH);
+       strcat(message, dataT);
+       free(dataT);
+
+       send(clientSocket,message,strlen(message),0);
+       close(clientSocket);
      }
-}
+  private:
+     int clientSocket;
+     sockaddr_in serverAddress;
+};
